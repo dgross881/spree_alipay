a7983ee317b95b407f998e34922aabb11d81fb62
dgross881 <dgross881@gmail.com>
2015-12-28 14:23:49 +0800

    fixed error in class

--- app/models/spree/spree_order_updater_decorator.rb
+++ app/models/spree/spree_order_updater_decorator.rb
@@ -10,7 +10,7 @@ Spree::OrderUpdater.class_eval do
     else
       order.payment_state = 'balance_due' if order.outstanding_balance > 0 && !order.payments.last.payment_method.kind_of?(alipay_base_class)
       order.payment_state = 'credit_owed' if order.outstanding_balance < 0
-      order.payment_state = 'paid' if !order.outstanding_balance? || order.payments.last.payment_method.kind_of?(alipay_base_clase)
+      order.payment_state = 'paid' if !order.outstanding_balance? || order.payments.last.payment_method.kind_of?(alipay_base_class)
     end
     order.state_changed('payment') if last_state != order.payment_state
     order.payment_state
