66e270f476dbb332f623d1799d974fb5beabf9c8
dgross881 <dgross881@gmail.com>
2015-12-28 14:22:47 +0800

    updated order updater decorator

--- app/models/spree/spree_order_updater_decorator.rb
+++ app/models/spree/spree_order_updater_decorator.rb
@@ -8,9 +8,9 @@ Spree::OrderUpdater.class_eval do
     elsif order.state == 'canceled' && order.payment_total == 0
       order.payment_state = 'void'
     else
-      order.payment_state = 'balance_due' if order.outstanding_balance > 0 && !order.payments.last.kind_of?(alipay_base_class)
+      order.payment_state = 'balance_due' if order.outstanding_balance > 0 && !order.payments.last.payment_method.kind_of?(alipay_base_class)
       order.payment_state = 'credit_owed' if order.outstanding_balance < 0
-      order.payment_state = 'paid' if !order.outstanding_balance? || order.payments.last.kind_of?(alipay_base_clase)
+      order.payment_state = 'paid' if !order.outstanding_balance? || order.payments.last.payment_method.kind_of?(alipay_base_clase)
     end
     order.state_changed('payment') if last_state != order.payment_state
     order.payment_state
