c8ccbb0781de44db7bd893aa74f745a97e6ccd8f
dgross881 <dgross881@gmail.com>
2016-03-02 15:47:14 +0700

    added ability to cancel an alipay order

--- app/models/spree/gateway/alipay_base.rb
+++ app/models/spree/gateway/alipay_base.rb
@@ -2,8 +2,6 @@ module Spree
     # start from Spree 3.0, class Gateway is removed
     class Gateway::AlipayBase < PaymentMethod
       # response_code -> trade_no:trade_status
-
-
       ServiceEnum = Struct.new( :trade_create_by_buyer,
         :create_direct_pay_by_user,
         :create_partner_trade_by_buyer,
@@ -17,5 +15,16 @@ module Spree
       def source_required?
         false
       end
+
+      def empty_success
+        Class.new do
+          def success?; true; end
+          def authorization; nil; end
+        end.new
+      end  
+
+      def cancel(response)
+        empty_success
+      end  
     end
 end
